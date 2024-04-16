486ca15fad0e2190a510c8e3b05958b099a54983
DavidAleksanyan1101 <166324203+DavidAleksanyan1101@users.noreply.github.com>
2024-04-16 17:29:57 +0400

    transpose matrix

---
+++ transposeMatrix.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+
+int* createMatrix(const int& n , const int& m) {
+    int* matrix = new int[n * m];
+    for (int i = 0; i < n * m; ++i) {
+            matrix[i] = rand() % 10;
+    }
+    return matrix;
+}
+
+
+void printMatrix(int* matrix , const int& n , const int& m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cout << matrix[i * m + j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void transposeMatrix(int* matrix , const int& size) {
+
+    for (int i = 1; i < size; ++i) {
+        for (int j = 0; j < i; ++j) {
+            std::swap(matrix[i * size + j] , matrix[j * size + i]);
+        }
+    }
+
+}
+
+
+
+int main() {
+    int* matrix1 = createMatrix(4 , 4);
+    printMatrix(matrix1 , 4 , 4);
+
+    std::cout<<"transpose matrix"<<std::endl;
+    transposeMatrix(matrix1 , 4);
+    printMatrix(matrix1 , 4 , 4);
+    
+
+    return 0;
+}
