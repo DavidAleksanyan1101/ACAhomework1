adc32e0d25bdc632c62c466568da63b6158d6253
DavidAleksanyan1101 <166324203+DavidAleksanyan1101@users.noreply.github.com>
2024-04-18 15:32:50 +0400

    Homework 14

---
+++ homework14.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+
+
+class Matrix
+{
+public:
+    Matrix() = default;
+    Matrix(const int& _n , const int& _m) : n(_n) , m(_m)
+    {
+        matrix = new int* [n];
+        for (int i = 0; i < n; ++i)
+        {
+            matrix[i] = new int[m];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                matrix[i][j] = rand() % 100 + 1;
+            }
+        }
+    }
+
+    void transposeMatrix()
+    {
+        Matrix newMatrix(m , n);
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                newMatrix.matrix[i][j] = matrix[j][i];
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            delete matrix[i];
+        }
+        delete[] matrix;
+        matrix = newMatrix.matrix;
+        std::swap(n , m);
+    }
+
+    void printMatrix()
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+private:
+    int** matrix = nullptr;
+    int n = 0;
+    int m = 0;
+};
+
+int main()
+{
+    Matrix matrix1(6 , 3);
+    matrix1.printMatrix();
+    std::cout<<std::endl;
+    matrix1.transposeMatrix();
+    matrix1.printMatrix();
+
+}
