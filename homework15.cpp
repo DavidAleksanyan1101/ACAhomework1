c065e6d8f0a00d0a13ed2d6ff3d7fa7c47996f01
DavidAleksanyan1101 <166324203+DavidAleksanyan1101@users.noreply.github.com>
2024-04-18 22:43:21 +0400

    Homework 15

---
+++ homework15.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+class Car {
+    std::string brand;
+    std::string model;
+    std::string color;
+    std::string engineType;
+    int hp; //horsepower
+    int maxSpeed;
+    int year;
+};
+
+int main()
+{
+    return 0;
+}
