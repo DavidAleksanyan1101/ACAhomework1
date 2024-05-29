7bb3692a27f229e9b4574001e16787c17a90ae9e
David <davidaleksanyan753@gmail.com>
2024-05-30 00:50:08 +0400

    vector

---
+++ STL_Implementation/vector/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include"vector.hpp"
+#include<array>
+
+int main()
+{
+    std::vector<int> v2;
+    myStd::vector<int> v1;
+    std::cout << v1.size() << std::endl;
+    std::cout << v1.capacity() << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        v1.push_back(i);
+    }
+    
+    for (int i = 0; i < v1.size(); i++)
+    {
+        std::cout << v1[i] << " " ;
+    }
+    std::cout<<std::endl;
+    v1.push_front(42);
+    v1.push_front(67);
+    v1.pop_front();
+    v1.insert(47 , 2);
+    v1.insert(7 , 4);
+    for (int i = 0; i < v1.size(); i++)
+    {
+        std::cout << v1[i] << " " ;
+    }
+
+
+    return 0;
+}
---
+++ STL_Implementation/vector/vector.hpp
@@ -0,0 +1,275 @@
+#ifndef __Vector__
+#define __Vector__
+
+namespace myStd
+{
+    template<typename T>
+    class vector
+    {
+    public:
+        vector() = default; //defult ctor
+        vector(const int&);//param ctor
+        vector(const vector&);//cpy ctor
+        vector(vector&&);//move ctor
+        vector& operator=(const vector&);//operator=
+        vector& operator=(vector&&);//move operator=
+        ~vector();
+    public:
+        int size();
+        int capacity();
+        void push_back(const T&);
+        void push_front(const T&);
+        void pop_back();
+        void pop_front();
+        void insert(const T& , const int&);
+        T& operator[](const int&);
+        T& at(const int&);
+        T& front();
+        T& back();
+    private:
+        T* _arr = nullptr;
+        int _capacity = 0;
+        int _size = 0;
+    };
+
+
+
+
+    template<typename T>
+    vector<T>::vector(const int& size) : _size(size) , _capacity(size)
+    {
+        std::cout << "param ctor" << std::endl;
+        if (size == 0)
+        {
+            _capacity = 0;
+            return;
+        }
+        _arr = new T[_capacity];
+    }
+    template<typename T>
+    vector<T>::vector(const vector& v) : _size(v._size) , _capacity(v._capacity)
+    {
+        std::cout << "cpy ctor" << std::endl;
+        if (_capacity == 0)
+        {
+            return;
+        }
+        _arr = new T[_capacity];
+        if (_size == 0)
+        {
+            return;
+        }
+        for (int i = 0; i < _size; i++)
+        {
+            _arr[i] = v._arr[i];
+        }
+    }
+
+    template<typename T>
+    vector<T>::vector(vector&& v)
+    {
+        std::cout << "move ctor" << std::endl;
+        _size = v._size;
+        _capacity = v._capacity;
+        _arr = v._arr;
+
+        v._arr = nullptr;
+        v._size = 0;
+        v._capacity = 0;
+    }
+
+    template<typename T>
+    vector<T>& vector<T>::operator=(const vector& v)
+    {
+        _size = v._size;
+        _capacity = v._capacity;
+        if (_capacity == 0)
+        {
+            _arr = nullptr;
+            return this;
+        }
+        _arr = new T[_capacity];
+        if (_size == 0)
+        {
+            return this;
+        }
+        for (int i = 0; i < _size; i++)
+        {
+            _arr[i] = v._arr[i];
+        }
+        return this;
+    }
+
+    template<typename T>
+    vector<T>& vector<T>::operator=(vector&& v)
+    {
+        delete[] _arr;
+        _size = v._size;
+        _capacity = v._capacity;
+        _arr = v._arr;
+
+        v._arr = nullptr;
+        _capacity = 0;
+        _size = 0;
+    }
+
+    template<typename T>
+    vector<T>::~vector()
+    {
+        delete[] _arr;
+    }
+
+    template<typename T>
+    int vector<T>::size()
+    {
+        return _size;
+    }
+
+    template<typename T>
+    int vector<T>::capacity()
+    {
+        return _capacity;
+    }
+
+    template<typename T>
+    T& vector<T>::operator[](const int& i)
+    {
+        return _arr[i];
+    }
+
+    template<typename T>
+    T& vector<T>::at(const int& i)
+    {
+        if (i >= _size)
+        {
+            throw std::out_of_range("Invalid index!");
+        }
+        return _arr[i];
+    }
+
+    template<typename T>
+    void vector<T>::push_back(const T& elem)
+    {
+        if (_size < _capacity)
+        {
+            _arr[_size] = elem;
+            ++_size;
+            return;
+        }
+        _capacity *= 2;
+        if (_capacity == 0)
+        {
+            _capacity = 1;
+        }
+        T* tmp = new T[_capacity];
+        for (int i = 0; i < _size; i++)
+        {
+            tmp[i] = _arr[i];
+        }
+        delete[] _arr;
+        _arr = tmp;
+        tmp = nullptr;
+        _arr[_size] = elem;
+        ++_size;
+        return;
+    }
+
+
+    template<typename T>
+    void vector<T>::pop_back()
+    {
+        if (_size != 0)
+        {
+            --_size;
+        }
+        return;
+    }
+
+    template<typename T>
+    void vector<T>::push_front(const T& t)
+    {
+        if (_size == _capacity)
+        {
+            _capacity *= 2;
+            if (_capacity == 0)
+            {
+                _capacity = 1;
+            }
+            T* tmp = new T[_capacity];
+            tmp[0] = t;
+            for (int i = 0; i < _size; i++)
+            {
+                tmp[i + 1] = _arr[i];
+            }
+            delete[] _arr;
+            _arr = tmp;
+            tmp = nullptr;
+            ++_size;
+            return;
+        }
+        for (int i = 0; i < _size; i++)
+        {
+            _arr[_size - i] = _arr[_size - i - 1];
+        }
+        _arr[0] = t;
+        ++_size;
+        return;
+    }
+
+    template<typename T>
+    void vector<T>::pop_front()
+    {
+        if (_size == 1)
+        {
+            --_size;
+        }
+        else if (_size != 0)
+        {
+            for (int i = 0; i < _size - 1; i++)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+            --_size;
+        }
+    }
+
+    template<typename T>
+    void vector<T>::insert(const T& t , const int& index)
+    {
+        if (_size == _capacity)
+        {
+            _capacity *= 2;
+            if (_capacity == 0)
+            {
+                _capacity = 1;
+            }
+            T* tmp = new T[_capacity];
+            for (int i = 0; i < _size; i++)
+            {
+                tmp[i] = _arr[i];
+            }
+            delete[] _arr;
+            _arr = tmp;
+            tmp = nullptr;
+        }
+        for (int i = 0; i < _size - index; i++)
+        {
+            _arr[_size - i] = _arr[_size - i - 1];
+        }
+        _arr[index] = t;
+        ++_size;
+        return;
+    }
+
+
+    template<typename T>
+    T& vector<T>::front()
+    {
+        if (_size != 0)
+        {
+            return _arr[0];
+        }
+    }
+
+};
+#endif //__Vector__
