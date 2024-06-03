62ea596f9ee6afe454633363d3d5a1c5a0b7dba5
David <davidaleksanyan753@gmail.com>
2024-06-03 11:39:09 +0400

    forward list update

--- STL_Implementation/forward_list.hpp
+++ STL_Implementation/forward_list.hpp
@@ -5,10 +5,10 @@ template <typename T>
 class Node
 {
 public:
-    Node(const T &);
+    Node(const T&);
 
 public:
-    Node *next = nullptr;
+    Node* next = nullptr;
     T data;
 };
 
@@ -17,32 +17,96 @@ class forward_list
 {
 public:
     forward_list() = default;
-    forward_list(const Node<T> &);
-    forward_list(const T &);
-
 public:
-    void push_front(const Node);
-
+    void push_front(const Node<T>&);
+    void push_front(const T&);
+    void pop_front();
+    void insert(const int& index , const T&);
+    void print();
 private:
-    Node<T> *head = nullptr;
+    Node<T>* head = nullptr;
 };
 
 template <typename T>
-Node<T>::Node(const T &_data)
+Node<T>::Node(const T& _data)
 {
     data = _data;
 }
 
-template <typename T>
-forward_list<T>::forward_list(const Node<T> &n)
+// template <typename T>
+// forward_list<T>::forward_list(const Node<T>& n)
+// {
+//     head = new Node<T>(n.data);
+// }
+
+// template <typename T>
+// forward_list<T>::forward_list(const T& n)
+// {
+//     head = new Node<T>(n);
+// }
+
+template<typename T>
+void forward_list<T>::push_front(const Node<T>& n)
 {
-    head = new Node(n.data);
+    if (head != nullptr)
+    {
+        Node<T>* tmp = new Node<T>(n.data);
+        tmp->next = head;
+        head = tmp;
+        return;
+    }
+    head = new Node<T>(n.data);
 }
 
-template <typename T>
-forward_list<T>::forward_list(const T& n)
+template<typename T>
+void forward_list<T>::push_front(const T& n)
 {
-    head = new Node(n.data);
+    if (head != nullptr)
+    {
+        Node<T>* tmp = new Node<T>(n);
+        tmp->next = head;
+        head = tmp;
+        return;
+    }
+    head = new Node<T>(n);
+}
+
+template<typename T>
+void forward_list<T>::pop_front()
+{
+    Node<T>* tmp = head;
+    head = tmp->next;
+    delete tmp;
+}
+template<typename T>
+void forward_list<T>::insert(const int& index , const T& t)
+{
+    if (index != 0)
+    {
+        Node<T>* tmp = head;
+        for (int i = 0; i < index - 1; i++)
+        {
+            tmp = tmp->next;
+        }
+        Node<T>* newNode = new Node<T>(t);
+        newNode->next = tmp->next;
+        tmp->next = newNode;
+        return;
+    }
+    this->push_front(t);
+}
+
+template<typename T>
+void forward_list<T>::print()
+{
+    if(head != nullptr){
+    std::cout<< head->data<<" ";
+    for (Node<T>* i = head; i->next != nullptr; i = i->next)
+    {
+        std::cout<< i->next->data<<" ";
+    }
+    std::cout<<std::endl;
+    }
 }
 
 #endif //__forward_list__
--- STL_Implementation/main.cpp
+++ STL_Implementation/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "mySTL.hpp"
+#include<forward_list>
 
 int main()
 {
-    mySTL::Node<int> n(5);
-    mySTL::forward_list<std::string> sl("adss5");
-
-
+    mySTL::Node<std::string> n("ddd5");
+    mySTL::forward_list<int> sl;
+    std::string s1 = "6";
+    sl.push_front(1);
+    sl.push_front(2);
+    sl.push_front(3);
+    sl.push_front(4);
+    sl.push_front(5);
+    sl.print();
+    sl.insert(0 , 45);
+    sl.print();
+    sl.pop_front();
+    sl.print();
     return 0;
 }
