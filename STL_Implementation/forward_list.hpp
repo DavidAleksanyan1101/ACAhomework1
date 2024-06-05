e3af7c52991a1622c1f87861722e05a040ccb1a4
davidaleksanyan <davidaleksanyan753@gmail.com>
2024-06-06 00:54:29 +0400

    list added

--- STL_Implementation/forward_list.hpp
+++ STL_Implementation/forward_list.hpp
@@ -8,10 +8,10 @@ namespace mySTL
     class Node
     {
     public:
-        Node(const T&);
+        Node(const T &);
 
     public:
-        Node* next = nullptr;
+        Node *next = nullptr;
         T data;
     };
 
@@ -20,44 +20,35 @@ namespace mySTL
     {
     public:
         forward_list() = default;
+
     public:
-        void push_front(const Node<T>&);
-        void push_front(const T&);
+        void push_front(const Node<T> &);
+        void push_front(const T &);
         void pop_front();
-        void insert(const int& index , const T&);
+        void insert(const int &index, const T &);
+        void makeCycle();
         bool cycleCheck();
+        T lastNthElem(const int &);
         void print();
         void reverse();
         int size();
+
     private:
-        Node<T>* head = nullptr;
+        Node<T> *head = nullptr;
     };
 
-
     template <typename T>
-    Node<T>::Node(const T& _data)
+    Node<T>::Node(const T &_data)
     {
         data = _data;
     }
 
-    // template <typename T>
-    // forward_list<T>::forward_list(const Node<T>& n)
-    // {
-    //     head = new Node<T>(n.data);
-    // }
-
-    // template <typename T>
-    // forward_list<T>::forward_list(const T& n)
-    // {
-    //     head = new Node<T>(n);
-    // }
-
-    template<typename T>
-    void forward_list<T>::push_front(const Node<T>& n)
+    template <typename T>
+    void forward_list<T>::push_front(const Node<T> &n)
     {
         if (head != nullptr)
         {
-            Node<T>* tmp = new Node<T>(n.data);
+            Node<T> *tmp = new Node<T>(n.data);
             tmp->next = head;
             head = tmp;
             return;
@@ -65,12 +56,12 @@ namespace mySTL
         head = new Node<T>(n.data);
     }
 
-    template<typename T>
-    void forward_list<T>::push_front(const T& n)
+    template <typename T>
+    void forward_list<T>::push_front(const T &n)
     {
         if (head != nullptr)
         {
-            Node<T>* tmp = new Node<T>(n);
+            Node<T> *tmp = new Node<T>(n);
             tmp->next = head;
             head = tmp;
             return;
@@ -78,24 +69,24 @@ namespace mySTL
         head = new Node<T>(n);
     }
 
-    template<typename T>
+    template <typename T>
     void forward_list<T>::pop_front()
     {
-        Node<T>* tmp = head;
+        Node<T> *tmp = head;
         head = tmp->next;
         delete tmp;
     }
-    template<typename T>
-    void forward_list<T>::insert(const int& index , const T& t)
+    template <typename T>
+    void forward_list<T>::insert(const int &index, const T &t)
     {
         if (index != 0)
         {
-            Node<T>* tmp = head;
+            Node<T> *tmp = head;
             for (int i = 0; i < index - 1; i++)
             {
                 tmp = tmp->next;
             }
-            Node<T>* newNode = new Node<T>(t);
+            Node<T> *newNode = new Node<T>(t);
             newNode->next = tmp->next;
             tmp->next = newNode;
             return;
@@ -103,11 +94,11 @@ namespace mySTL
         this->push_front(t);
     }
 
-    template<typename T>
+    template <typename T>
     bool forward_list<T>::cycleCheck()
     {
-        Node<T>* first = head->next;
-        Node<T>* second = head->next->next;
+        Node<T> *first = head->next;
+        Node<T> *second = head->next->next;
         while (first != nullptr || second != nullptr)
         {
             if (first == second)
@@ -120,13 +111,13 @@ namespace mySTL
         return false;
     }
 
-    template<typename T>
+    template <typename T>
     int forward_list<T>::size()
     {
         int count = 0;
         if (head != nullptr)
         {
-            Node<T>* tmp = head;
+            Node<T> *tmp = head;
             while (tmp != nullptr)
             {
                 ++count;
@@ -136,13 +127,13 @@ namespace mySTL
         return count;
     }
 
-    template<typename T>
+    template <typename T>
     void forward_list<T>::print()
     {
         if (head != nullptr)
         {
             std::cout << head->data << " ";
-            for (Node<T>* i = head; i->next != nullptr; i = i->next)
+            for (Node<T> *i = head; i->next != nullptr; i = i->next)
             {
                 std::cout << i->next->data << " ";
             }
@@ -150,13 +141,13 @@ namespace mySTL
         }
     }
 
-    template<typename T>
+    template <typename T>
     void forward_list<T>::reverse()
     {
         if (head != nullptr)
         {
             std::stack<T> st;
-            Node<T>* tmp = head;
+            Node<T> *tmp = head;
             while (true)
             {
                 st.push(tmp->data);
@@ -182,5 +173,24 @@ namespace mySTL
         }
     }
 
+template <typename T>
+T forward_list<T>::lastNthElem(const int &a)
+{
+    if (a >= this->size())
+    {
+        throw std::out_of_range(" ");
+    }
+    
+    Node<T>* tmp = head;
+    for (int i = 0; i < this->size() - a - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    return tmp->data;
+}
+
+
 };
+
+
 #endif //__forward_list__
