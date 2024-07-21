3f64df23eb9522391450aa90de039b516584fe8d
davidaleksanyan <davidaleksanyan753@gmail.com>
2024-07-21 22:33:34 +0400

    matrix multithread sum

--- matrixSumMultitread.cpp
+++ matrixSumMultitread.cpp
@@ -1,27 +1,62 @@
-#include<iostream>
+#include<bits/stdc++.h>
 #include<thread>
 
-void f(){
-
-    std::cout<<f<<std::endl;
+int columnSum(int** mat , int size , int& sum , int begin , int end)
+{
+    sum = 0;
+    for (int i = begin; i < end; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            sum += mat[j][i];
+        }
+    }
+    return sum;
 }
-
-int main(){
-    int matrix[1000][1000];
-    std::thread t1(f);
+int main()
+{
     auto start = std::chrono::high_resolution_clock::now();
+    int size = 10;
+    int** matrix = new int* [size];
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = new int[size];
+    }
 
-    int sum = 0;
-    for(int i = 0 ; i < 1000 ; i++){
-        for(int j = 0 ; j< 1000 ; j++){
-            sum += matrix[i][j];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i][j] = i * 10 + j;
         }
     }
 
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     for (int j = 0; j < size; j++)
+    //     {
+    //         std::cout << matrix[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+
+    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+
+    std::thread t1(columnSum , std::ref(matrix) , size , std::ref(sum1) , 0 , size / 2);
+    std::thread t2(columnSum , std::ref(matrix) , size , std::ref(sum2) , size / 2 , size);
+
+    t1.join();
+    t2.join();
+    sum = sum1 + sum2;
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout<<sum<<std::endl;
+    std::cout << sum << std::endl;
 
-    std::cout << "Elapsed time: " << elapsed_seconds.count() << " seconds\n";
+    std::cout << "Elapsed time: " << std::fixed << std::setprecision(10) <<
+        elapsed_seconds.count() << " seconds\n";
 }
